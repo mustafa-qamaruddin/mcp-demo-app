74c1926b1d8f4d58f40612b3902f79bfb88ead6e
Mustafa Qamar-ud-Din <mustafa.mahrous.89@gmail.com>
2026-05-16 16:42:49 +0200

    Add repeater

--- src/main/java/org/mqubits/Repeater.java
+++ src/main/java/org/mqubits/Repeater.java
@@ -1,11 +1,48 @@
 package org.mqubits;
 
 import io.modelcontextprotocol.server.McpServerFeatures;
+import io.modelcontextprotocol.server.McpSyncServerExchange;
+import io.modelcontextprotocol.spec.McpSchema;
+
+import java.util.List;
+import java.util.Map;
+import java.util.function.BiFunction;
 
 public class Repeater {
   public McpServerFeatures.SyncToolSpecification repeat() {
-    return new McpServerFeatures.SyncToolSpecification(
 
-    )
+    Map<String, Object> properties = Map.of(
+      "prompt", String.class
+    );
+
+    List<String> required = List.of("prompt");
+
+    McpSchema.JsonSchema mcpSchema = new McpSchema.JsonSchema(
+      "object",
+      properties,
+      required,
+      false,
+      null,
+      null
+    );
+
+    McpSchema.Tool tool = new McpSchema.Tool("repeater", "Repeater", "Just another repeater",
+      mcpSchema, null, null, null);
+
+    return new McpServerFeatures.SyncToolSpecification(
+      tool,
+      (exchange, req) -> {
+        String prompt = (String) req.arguments().get("prompt");
+        McpSchema.TextContent textContent = new McpSchema.TextContent(
+          new StringBuilder().append("Prompt: ").append(prompt).toString()
+        );
+        McpSchema.CallToolResult result = McpSchema.CallToolResult.builder()
+          .content(List.of(extContent))
+          .isError(false)
+          .build();
+
+        return result;
+    }
+    );
   }
 }
